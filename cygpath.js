f46888c4970a0e224ba76b95bf5e3aab78d17d5c
robario <webmaster@robario.com>
2016-09-28 13:58:49 +0900

    :bug: Fix unexpected option used even if it's falsy value

--- cygpath.js
+++ cygpath.js
@@ -9,9 +9,11 @@ function cygpath(name, options) {
     const args = [];
     Object.keys(options || {})
         .forEach((key) => {
-            args.push((1 < key.length ? '--' : '-') + key);
-            if (options[key] && options[key].constructor === String) {
-                args.push(options[key]);
+            if (options[key]) {
+                args.push((1 < key.length ? '--' : '-') + key);
+                if (options[key].constructor === String) {
+                    args.push(options[key]);
+                }
             }
         });
     args.push('--', name);
